0fc59e067a136731f44660ede3623edf74131aed
lanycsq <shanqiong118@163.com>
2020-12-21 22:32:03 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+ import Vue from 'vue'
+ // import App from './App.vue'
 
-Vue.config.productionTip = false
+ // Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+ // new Vue({
+ //   render: h => h(App),
+ // }).$mount('#app')
+ // console.log(window.Vue);
+
+ import Demo from './Demo.vue'
+ new Vue({
+   el: "#app",
+   render(h) {
+     return h(Demo)
+   },
+ })
